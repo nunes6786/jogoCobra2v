b9d595c509ea561b428db19a8c042adef27699d6
nunes6786 <arthurnunes6786@gmail.com>
2025-12-10 10:13:12 -0300

    Jogo da Cobra

--- js/tela.js
+++ js/tela.js
@@ -1,7 +1,7 @@
 const tela = {
     largura: 360,
     altura: 600,
-    cor: "#85d656ff",
+    cor: "#000000ff",
     desenhar(){
         ctx.fillStyle = this.cor;
         ctx.fillRect(0,30,this.largura,this.altura)
