d4c191cf0f9fd210d2c8709fe8d6fe256311af8d
nunes6786 <arthurnunes6786@gmail.com>
2025-12-10 10:12:05 -0300

    Jogo da Cobra

--- js/placar.js
+++ js/placar.js
@@ -1,10 +1,10 @@
 const placar = {
     largura:360,
     altura:30,
-    corFundo:"blue",
-    corTexto:"white",
+    corFundo:"lightgreen",
+    corTexto:"darkblue",
     pontuacao:0,
-    nomeJogo:"Snake Game",
+    nomeJogo:"Snake Game IFRJ",
     desenhar(){
         ctx.fillStyle = this.corFundo;
         ctx.fillRect(0,0,this.largura,this.altura);
